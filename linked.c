2f79c08e9906ce0ff114f67d027d3d207b663ce6
Satyam-agarwal <agar1satyam.com>
2018-02-27 15:40:58 +0530

    malloc problem sorted

--- linked.c
+++ linked.c
@@ -11,57 +11,55 @@ int main()
 		struct NODE *ptr;/* num */
 	};
 	typedef struct NODE NODE ;
-    NODE *head,*temp2,*temp =0;
+    NODE *head,*temp1,*temp ;
 
     head = (struct NODE*)malloc(sizeof(struct NODE)); 
-  temp2 = (struct NODE*)malloc(sizeof(struct NODE));
-  temp = (struct NODE*)malloc(sizeof(struct NODE));
+  
+  
   
    int choice,input;
    printf("Enter your choice 0:add 1:quit\n");
    
 
-   NODE* create(int num,NODE* ptr)
-   
+          head->ptr=NULL;
+           
 
-   {NODE* new_node = (NODE*)malloc(sizeof(NODE));
-    if(new_node == NULL)
-    {
-        printf("Error creating a new node.\n");
-        exit(0);
-    }
-    new_node->num = num;
-    new_node->ptr = ptr;
- 
-    return new_node;
-  }
-                                        NODE* append(NODE* head, int num)
-                                        {
-                                            if(head == NULL)
-                                                return NULL;
-                                            /* go to the last node */
-                                            NODE *cursor = head;
-                                            while(cursor->ptr != NULL)
-                                                cursor = cursor->ptr;
-                                         
-                                            /* create a new node */
-                                            NODE* new_node =  create(num,NULL);
-                                            cursor->ptr = new_node;
-                                         
-                                            return head;
-                                        }
+               void createnode(int data){
+
+                if(head->ptr==NULL)
+                  {temp = (struct NODE*)malloc(sizeof(struct NODE));   /* every time this malloc need to be created no only once */
+                    temp->num=data;
+                  
+                  head->ptr=temp;
+                  temp->ptr=NULL;
+
+                  printf("if side%p",head->ptr);
+                 } else
+                  {
+                    temp1 = (struct NODE*)malloc(sizeof(struct NODE));    /* every time this malloc need to be created no only once */
+                    temp1->num=data;
+                    
+                    temp->ptr=temp1;
+                    temp1->ptr=NULL;
+                    temp=temp1;
+                    printf("else side %p",head->ptr);
+                  }
+
+               printf("%p",head->ptr);
+               }
   
                              
            void diplay()
-                      { temp2=head->ptr;
-                        for(int i=0;i<3;i++)
+                      { temp=head->ptr;
+                        while((temp->ptr)!=NULL)
                         {
-                          printf("%d\n",temp2->num );
-                          temp2=temp2->ptr;
+                          printf("%d\n", temp->num);
+                          temp=temp->ptr;
                         }
+                        printf("%d\n", temp->num);
                       }
 
-          while(1){
+          while(1){ 
                       printf("enter choice\n");
                       scanf("%d",&choice);
         switch(choice) {
@@ -69,6 +67,7 @@ int main()
         
                   case 0: printf("enter\n");
                           scanf("%d",&input);
+                          createnode(input);
                           
                           break;
                   case 1: diplay();
